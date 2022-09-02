396342162c464daa12a176e1a22955b56cd29828
Lisandro Falconi <lfalconi.consultant@newscorp.com>
2022-09-02 10:44:00 -0300

    Initial Commit

---
+++ ChartsUI/ChartsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsUIApp.swift
+//  ChartsUI
+//
+//  Created by Lisandro Falconi on 02/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChartsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
